2b32366ea8cede41e8615b9ee17a97eb12ab52eb
hyperboria <anywayaura@gmail.com>
2022-07-24 23:45:43 +0700

    environment variables bot message clarified removed bot polling

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 import requests
 from datetime import datetime
 import os
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot
 import asyncio
 
 TOKEN = os.getenv('DVMNTKN')
 DVMN_NTF_BOT = os.getenv('dvmn_ntf_bot')
+MY_TG_ID = os.getenv('MY_TG_ID')
 
 bot = Bot(token=DVMN_NTF_BOT)
-dp = Dispatcher(bot)
-admin = 106901721
-
-
-@dp.message_handler(commands=['works'])
-async def get_work_list(message: types.message):
-    headers = {'Authorization': TOKEN}
-    response = requests.get('https://dvmn.org/api/user_reviews/', headers=headers)
-    response.raise_for_status()
-
-    for work in response.json()['results']:
-        submit_date = datetime.fromisoformat(work["submitted_at"]).strftime("%d.%m.%y %H:%M")
-        lesson_title = work["lesson_title"]
-        if work['is_negative']:
-            result = 'Доработки'
-        else:
-            result = 'Принято'
-        await bot.send_message(admin, f'{submit_date} {lesson_title} | {result}')
-        # print(f'{submit_date} {lesson_title} | {result}')
+admin = MY_TG_ID
 
 
 async def long_polling():
@@ -40,8 +23,14 @@ async def long_polling():
                 params = {"timestamp": response.json()["timestamp_to_request"]}
             elif response.json()["status"] == "found":
                 params = {"timestamp": response.json()["last_attempt_timestamp"]}
-                await bot.send_message(admin, response.json())
-                print(response.json()['request_query'])
+                for work in response.json()['new_attempts']:
+                    lesson_title = work["lesson_title"]
+                    if work['is_negative']:
+                        result = 'Нужна доработка'
+                    else:
+                        result = 'Успех!'
+                    url = work['lesson_url']
+                    await bot.send_message(admin, f'Работа "{lesson_title}" проверена: {result}\n{url}')
         except requests.exceptions.ReadTimeout as _exto:
             print(f'Timeout error ({_exto})')
         except ConnectionError as _exce:
@@ -49,11 +38,8 @@ async def long_polling():
         await asyncio.sleep(5)
 
 
-async def on_startup(_):
-    asyncio.create_task(long_polling())
-
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    asyncio.run(long_polling())
 
 
 

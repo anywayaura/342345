c3148b1d482a6312cbf9ff4235932445192e1a33
hyperboria <anywayaura@gmail.com>
2022-07-28 20:59:58 +0700

    prints -> logging variables precision exception timeout pass

--- main.py
+++ main.py
@@ -12,36 +12,36 @@ TG_NOTIFY_BOT = os.getenv('DVMN_REVIEWS_NOTIFY_TG_BOT')  # telegram bot token
 TG_CHAT_ID = os.getenv('TG_CHAT_ID')  # telegram chat id to notify
 
 
-async def dvmn_long_polling(DVMN_API_TOKEN, TG_NOTIFY_BOT, TG_CHAT_ID):
-    bot = Bot(token=TG_NOTIFY_BOT)
+async def dvmn_long_polling(dvmn_api_token, tg_notify_bot, tg_chat_id):
+    bot = Bot(token=tg_notify_bot)
     params = dict()
-    headers = {'Authorization': DVMN_API_TOKEN}
-    print('Polling reviews...')
+    headers = {'Authorization': dvmn_api_token}
     async with aiohttp.ClientSession() as session:
+        logging.info('Polling Reviews...')
         while True:
             try:
-                async with session.get('https://dvmn.org/api/long_polling/',
-                                       headers=headers, params=params) as response:
+                async with session.get(
+                        'https://dvmn.org/api/long_polling/', headers=headers, params=params) as response:
                     response.raise_for_status()
-                    data = await response.json()
-                    if data["status"] == "timeout":
-                        params = {"timestamp": data["timestamp_to_request"]}
-                    elif data["status"] == "found":
-                        params = {"timestamp": data["last_attempt_timestamp"]}
-                        for work in data['new_attempts']:
+                    reviews_json = await response.json()
+                    if reviews_json["status"] == "timeout":
+                        params = {"timestamp": reviews_json["timestamp_to_request"]}
+                    elif reviews_json["status"] == "found":
+                        params = {"timestamp": reviews_json["last_attempt_timestamp"]}
+                        for work in reviews_json['new_attempts']:
                             lesson_title = work["lesson_title"]
                             if work['is_negative']:
                                 result = 'Нужна доработка'
                             else:
                                 result = 'Успех!'
                             url = work['lesson_url']
-                            await bot.send_message(TG_CHAT_ID, f'Работа "{lesson_title}" проверена: {result}\n{url}')
+                            await bot.send_message(tg_chat_id, f'Работа "{lesson_title}" проверена: {result}\n{url}')
 
             except asyncio.exceptions.TimeoutError:
-                return None
+                pass
 
             except ConnectionError as _exce:
-                print(f'No internet connection ({_exce})')
+                logging.error(f'No internet connection ({_exce})')
                 await asyncio.sleep(10)
 
 
